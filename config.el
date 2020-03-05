8ea2a83c81e4f31e424d94030a98fa8809767566
Prajwal Rao <prajwaljpj@gmail.com>
2020-03-05 15:14:49 +0530

    Updated Readme.org file with requirements

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; These are used for a number of things, particularly for GPG configuration,
 ;; some email clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Prajwal Rao"
+      user-mail-address "prajwaljpj@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
