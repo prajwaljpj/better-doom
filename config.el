ba14eb77b3b819912d4caba90bb02685cce1440b
Prajwal Rao <prajwaljpj@gmail.com>
2020-03-06 09:30:36 +0530

    Dropbox supported capture template

--- config.el
+++ config.el
@@ -29,6 +29,56 @@
 (setq org-directory "~/Dropbox/org")
 (setq org-agenda-files '("~/Dropbox/org"))
 
+;; My capture template (TODO You can change them accordingly)
+
+;; (setq org-capture-templates
+;;       '(("t" "Todo" entry (file+headline "~/Dropbox/org/gtd.org" "Tasks")
+;;          "* TODO %?\n  %i\n  %a")
+;;         ("j" "Journal" entry (file+datetree "~/Dropbox/org/journal.org")
+;;          "* %?\nEntered on %U\n  %i\n  %a")))
+
+(setq org-capture-templates
+      '(("t" "All TODOs")
+ ("tp" "Personal todo" entry
+  (file+headline +org-capture-todo-file "Personal Tasks")
+  "* [ ] %?\n%i\n%a" :prepend t)
+ ("tw" "Work todo" entry
+  (file+headline +org-capture-todo-file "Professional Tasks")
+  "* [ ] %?\n%i\n%a" :prepend t)
+ ("ti" "Ideas" entry
+  (file+headline +org-capture-todo-file "Ideas")
+  "* [ ] %?\n%i\n%a" :prepend t)
+ ("n" "All Notes")
+ ("np" "Personal notes" entry
+  (file+headline +org-capture-notes-file "Personal Notes")
+  "* %u %?\n%i\n%a" :prepend t)
+ ("nw" "Work notes" entry
+  (file+headline +org-capture-notes-file "Professional Notes")
+  "* %u %?\n%i\n%a" :prepend t)
+ ("ni" "Interesting" entry
+  (file+headline +org-capture-notes-file "Interesting")
+  "* %u %?\n%i\n%a" :prepend t)
+ ("nl" "For Later" entry
+  (file+headline +org-capture-notes-file "Look Later")
+  "* %u %?\n%i\n%a" :prepend t)
+ ("j" "Journal" entry
+  (file+olp+datetree +org-capture-journal-file)
+  "* %U %?\n%i\n%a" :prepend t)
+ ("p" "Templates for projects")
+ ("pt" "Project-local todo" entry
+  (file+headline +org-capture-project-todo-file "Inbox")
+  "* TODO %?\n%i\n%a" :prepend t)
+ ("pn" "Project-local notes" entry
+  (file+headline +org-capture-project-notes-file "Inbox")
+  "* %U %?\n%i\n%a" :prepend t)
+ ("pc" "Project-local changelog" entry
+  (file+headline +org-capture-project-changelog-file "Unreleased")
+  "* %U %?\n%i\n%a" :prepend t)
+ ("o" "Centralized templates for projects")
+ ("ot" "Project todo" entry #'+org-capture-central-project-todo-file "* TODO %?\n %i\n %a" :heading "Tasks" :prepend nil)
+ ("on" "Project notes" entry #'+org-capture-central-project-notes-file "* %U %?\n %i\n %a" :heading "Notes" :prepend t)
+ ("oc" "Project changelog" entry #'+org-capture-central-project-changelog-file "* %U %?\n %i\n %a" :heading "Changelog" :prepend t)))
+
 ;; Some custom commands for agenda view
 ;; for more information follow this link
 ;; https://blog.aaronbieber.com/2016/09/24/an-agenda-for-life-with-org-mode.html
